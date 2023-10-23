11f83a0e7f7e46f32f774a1f01df78a223c81d29
Kartik Neti <kartikprajak@gmail.com>
2023-10-23 16:09:13 +0530

    Added email feature to MVP

--- main.py
+++ main.py
@@ -2,17 +2,19 @@ import requests
 import send_email as m
 
 api_key = "a36aaf886fb44ed8b954610544254aa7"
-url = 'https://newsapi.org/v2/everything?q=tesla&from=2023-09-23&sortBy=publishedAt\
-        &apiKey=a36aaf886fb44ed8b954610544254aa7'
+topic = 'zomato'
+url = f'https://newsapi.org/v2/everything?q={topic}&from=2023-09-23&sortBy=publishedAt&apiKey=a36aaf886fb44ed8b954610544254aa7&language=en'
     # api key = a36aaf886fb44ed8b954610544254aa7
 
 mail = ''
 request = requests.get(url)
 content = request.json()
-for article in content['articles']:
+for article in content['articles'][:1]:
     if article['title'] is not None:
-        mail = mail + article['title'] + '\n' + article['description'] + 2*'\n'
+        mail =  mail + article['title'] + '\n' + article['description'] + '\n' + article['url'] + 2*'\n'
+        
 
+mail = "Subject: Today's news" +'\n' + mail
 mail = mail.encode('utf-8')
 
 m.send_email(message= mail)
